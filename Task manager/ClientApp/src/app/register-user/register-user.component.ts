37f101773114b0ad3436de530d0b97a80006ae80
Artem <artemermolaev0@gmail.com>
2021-05-27 12:37:08 +0300

    Update register-user.component.ts

--- Task manager/ClientApp/src/app/register-user/register-user.component.ts
+++ Task manager/ClientApp/src/app/register-user/register-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormArray, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register-user',
@@ -6,10 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-user.component.css']
 })
 export class RegisterUserComponent implements OnInit {
+  registrationApplication: RegistrationApplication ;
+  public fb: FormBuilder;
 
-  constructor() { }
+  registrationApplicationForm: FormGroup = this.formBuilder.group({
+    email: ['', Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')],
+    passwrod: ['', Validators.required],
+  })
 
-  ngOnInit() {
+
+  constructor(private formBuilder: FormBuilder) {
   }
+
+
+  public isControlInvalid(controllName: string): boolean {
+    let control = this.registrationApplicationForm.get(controllName);
+
+    return !control.value;
+
+  }
+
+    public onSubmit() {
+      this.registrationApplicationForm = this.registrationApplicationForm.value;
+
+      localStorage.setItem('promotionApplication', JSON.stringify(this.registrationApplication));
+
+  }
+  private populateForm() {
+    this.registrationApplication = JSON.parse(localStorage.getItem('promotionApplication'));
+
+    if (this.registrationApplication) {
+      this.registrationApplicationForm.setValue(this.registrationApplication);
+    }
+  }
+ngOnInit() {
+}
+
+}
+export class RegistrationApplication {
+  email: string;
+  password: string;
 
 }
