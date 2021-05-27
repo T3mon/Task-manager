1484527aa4bb22e62c99216005737c7ebc0aca2f
Artem <artemermolaev0@gmail.com>
2021-05-27 11:23:06 +0300

    reactive forms in register

--- Task manager/ClientApp/src/app/app.module.ts
+++ Task manager/ClientApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
@@ -9,6 +9,7 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { LoginUserComponent } from './login-user/login-user.component';
+import { RegisterUserComponent } from './register-user/register-user.component';
 
 @NgModule({
   declarations: [
@@ -16,15 +17,18 @@ import { LoginUserComponent } from './login-user/login-user.component';
     NavMenuComponent,
     HomeComponent,
     FetchDataComponent,
-    LoginUserComponent
+    LoginUserComponent,
+    RegisterUserComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'login', component: LoginUserComponent },
+      { path: 'register', component: RegisterUserComponent },
       { path: 'fetch-data', component: FetchDataComponent },
     ])
   ],
--- Task manager/ClientApp/src/app/login-user/login-user.component.ts
+++ Task manager/ClientApp/src/app/login-user/login-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login-user',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-user.component.css']
 })
 export class LoginUserComponent implements OnInit {
+
 
   constructor() { }
 
